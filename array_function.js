8d8d3eb2fd4c2d5df8dae2b20ee64026c3d92171
Marrockx <mariam.adekola10@gmail.com>
2021-05-28 11:20:06 +0100

    logic-update

--- array_function.js
+++ array_function.js
@@ -1,25 +1,23 @@
 function checkYuGiOh(n) {
-  // input = true;
   var arr = Array.from({ length: n }, (_, i) => i + 1);
   // var arr = Array.from(Array(n + 1).keys()).slice(1);
-  // if (arr[i] % 2 == 0) {
-  //   arr[i] = "yu";
-  // } else if (arr[i] % 3 == 0) {
-  //   arr[i] = "gi";
-  // } else if (arr[i] % 5 == 0) {
-  //   arr[i] = "oh";
-  // }
-  return arr;
+  if (typeof parseInt(n) === "number") {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 == 0) {
+        arr[i] =
+          "yu" +
+          (arr[i] % 3 == 0 ? "-gi" : "") +
+          (arr[i] % 5 == 0 ? "-oh" : "");
+      } else if (arr[i] % 3 == 0) {
+        arr[i] = "gi" + (arr[i] % 5 == 0 ? "-oh" : "");
+      } else if (arr[i] % 5 == 0) {
+        arr[i] = "oh";
+      }
+    }
+    return arr;
+  } else {
+    return `invalid parameter: "${n}"`;
+  }
 }
 
-console.log(checkYuGiOh(8));
-
-//     - for numbers that have multiple factors, use hyphens as separators
-//     e.g 10 === "yu-oh", 30 === "yu-gi-oh"
-//     - perform checks on your input and return `invalid parameter: ${parameter}` if an invalid parameter - i.e. a string that can't be converted to a number or another data type - is passed.
-
-// Use the following cases as guides:
-
-//     - checkYuGiOh(10) should return [1, "yu", "gi", "yu", "oh", "yu-gi", 7, "yu", "gi", "yu-oh"]
-//     - checkYuGiOh("5") should return [1, "yu", "gi", "yu", "oh"]
-//     - checkYuGiOh("fizzbuzz is meh") should return `invalid parameter: "fizzbuzz is meh"
+console.log(checkYuGiOh(20));
